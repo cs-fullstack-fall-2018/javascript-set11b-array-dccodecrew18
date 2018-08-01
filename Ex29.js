c090cb1bb5d0cf6048fcfaa7d3df26dd0432f3ca
Darius Carter <dcarterbusinessemail@gmail.com>
2018-08-01 15:05:05 -0500

    Finished

---
+++ Ex29.js
@@ -0,0 +1,6 @@
+var numberList = [2,6,81];
+numberList.push(1);
+numberList.push(3);
+
+console.log(numberList);
+
---
+++ Ex30.js
@@ -0,0 +1,4 @@
+var bestArray = ["Sonic", "The", "Blue", "Fast", "Blur"];
+console.log(bestArray[2]);
+console.log(bestArray.splice(1,1));
+console.log(bestArray[2]);
